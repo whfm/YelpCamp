617df2f741894d67f3ab27242c524b8dd8f77213
Walter <35459002+whfm@users.noreply.github.com>
2018-08-28 18:31:08 -0400

    Update index.js

--- middleware/index.js
+++ middleware/index.js
@@ -26,20 +26,25 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
     }
 };
 
-middlewareObj.checkCommentOwnership = function(req, res, next){
-    Comment.findById(req.params.commentId, function(err, foundComment){
-       if(err || !foundComment){
-           console.log(err);
-           req.flash('error', 'Sorry, the comment does not exist!');
-           res.redirect('/campgrounds');
-       } else if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
-            req.comment = foundComment;
-            next();
-       } else {
-           req.flash('error', 'You don\'t have permission to do that!');
-           res.redirect('/campgrounds/' + req.params.id);
-       }
-    });
+middlewareObj.checkCommentOwnership = function(req, res, next) {
+ if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+           if(err){
+               res.redirect("back");
+           }  else {
+               // does user own the comment?
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission to do that");
+                res.redirect("back");
+            }
+           }
+        });
+    } else {
+        req.flash("error", "You need to be logged in to do that");
+        res.redirect("back");
+    }
 }
 
 middlewareObj.isLoggedIn = function(req, res, next) {
